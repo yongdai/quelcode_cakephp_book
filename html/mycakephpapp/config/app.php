87dd25223945ca886b3fd33a95182a086d72c1b3
yongdai <yongdai777@gmail.com>
2020-06-19 13:01:44 +0900

    mysqlのIPアドレス追加

--- html/mycakephpapp/config/app.php
+++ html/mycakephpapp/config/app.php
@@ -256,7 +256,7 @@ return [
             'className' => Connection::class,
             'driver' => Mysql::class,
             'persistent' => false,
-            'host' => 'mysql',
+            'host' => '172.22.0.4',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
